b12a2c97ec4d7efc5e08f6b39c3b06c5409334e7
asijaharshit <asijaharshit@gmail.com>
2026-01-07 12:25:38 +0400

    Solved Problem 25

---
+++ dsa/prb25_upper_bound.py
@@ -0,0 +1,15 @@
+arr = [1,1,1,2,3,4,4,5,6,7]
+
+l = 0
+h = len(arr)-1
+
+no=1
+
+while l <=h:
+    mid = (l+h)//2
+    if arr[mid] >no:
+        h=mid-1
+    else:
+        l=mid+1
+
+print("Lowerbound the no",l)
